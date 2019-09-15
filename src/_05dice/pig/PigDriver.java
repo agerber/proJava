ba4617c0ee29af98ac854cf854a89600e05e89c0
Adam Gerber <adam.gerber@yahoo.com>
2019-09-15 15:52:28 -0500

    clean up files

---
+++ src/_05dice/pig/PigDriver.java
@@ -0,0 +1,9 @@
+package _05dice.pig;
+
+public class PigDriver {
+
+    public static void main(String[] args) {
+        //pig game here
+
+    }
+}
--- src/_05dice/pig/Stub.java
+++
@@ -1,6 +0,0 @@
-package _05dice.pig;
-
-
-public class Stub {
-    //nothing implemented here
-}
