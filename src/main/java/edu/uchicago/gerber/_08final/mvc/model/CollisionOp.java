bfdc497d7d70d76800df3006375adb696a0af424
Adam Gerber <adam.gerber@yahoo.com>
2021-11-18 13:09:52 -0600

    collisionOp using @AllArgsConstructor

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/CollisionOp.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/CollisionOp.java
@@ -1,35 +1,23 @@
 package edu.uchicago.gerber._08final.mvc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * Created by ag on 6/17/2015.
  */
 
-
+//the lombok @Data gives us automatic getters and setters
+@Data
+//the lombok @AllArgsConstructor gives us an All-Args-Constructor :)
+@AllArgsConstructor
 public class CollisionOp {
-
     //this could also be a boolean, but we want to be explicit about what we're doing
     public enum Operation {
         ADD, REMOVE
     }
-
     //members
     private Movable movable;
     private Operation operation;
-
-    //constructor
-    public CollisionOp(Movable movable, Operation op) {
-        this.movable = movable;
-        this.operation = op;
-    }
-
-
-    //getters
-    public Movable getMovable() {
-        return movable;
-    }
-
-    public Operation getOperation() {
-        return operation;
-    }
 
 }
