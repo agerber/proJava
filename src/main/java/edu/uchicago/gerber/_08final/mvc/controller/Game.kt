83daa23ab5476e1253cd2aa218adbc8ebb4e35c5
Adam Gerber <adam.gerber@yahoo.com>
2022-08-03 21:59:36 -0500

    working but need to jump start it, check the boolean value and set to false initially

--- src/main/java/edu/uchicago/gerber/_08final/mvc/controller/Game.kt
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/controller/Game.kt
@@ -14,6 +14,7 @@ import javax.sound.sampled.Clip
 fun main() {
     //typical Swing application start; we pass EventQueue a Runnable object.
     EventQueue.invokeLater(Game())
+    CommandCenter.initGame()
 }
 // ===============================================
 // == This Game class is the CONTROLLER
