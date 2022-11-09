fe64409d2dc5b4ff5ef325441689fa4423990ce2
Adam Gerber <adam.gerber@yahoo.com>
2022-11-09 11:03:51 -0600

    refactor game instructions again

--- src/main/java/edu/uchicago/gerber/_08final/mvc/view/GamePanel.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/view/GamePanel.java
@@ -9,181 +9,184 @@ import java.awt.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.BiConsumer;
-
+import java.util.function.Consumer;
 
 
 public class GamePanel extends Panel {
-	
-	// ==============================================================
-	// FIELDS 
-	// ============================================================== 
-	 
-	// The following "off" vars are used for the off-screen double-buffered image.
-	private Image imgOff;
-	private Graphics grpOff;
-	
-	private GameFrame gmf;
-	private Font fnt = new Font("SansSerif", Font.BOLD, 12);
-	private Font fntBig = new Font("SansSerif", Font.BOLD + Font.ITALIC, 36);
-	private FontMetrics fmt; 
-	private int fontWidth;
-	private int fontHeight;
-	private String strDisplay = "";
-	
 
-	// ==============================================================
-	// CONSTRUCTOR 
-	// ==============================================================
-	
-	public GamePanel(Dimension dim){
-	    gmf = new GameFrame();
-		gmf.getContentPane().add(this);
-		gmf.pack();
-		initView();
-		
-		gmf.setSize(dim);
-		gmf.setTitle("Game Base");
-		gmf.setResizable(false);
-		gmf.setVisible(true);
-		setFocusable(true);
-	}
-	
-	
-	// ==============================================================
-	// METHODS 
-	// ==============================================================
-	
-	private void drawScore(Graphics g) {
-		g.setColor(Color.white);
-		g.setFont(fnt);
-		if (CommandCenter.getInstance().getScore() != 0) {
-			g.drawString("SCORE :  " + CommandCenter.getInstance().getScore(), fontWidth, fontHeight);
-		} else {
-			g.drawString("NO SCORE", fontWidth, fontHeight);
-		}
-	}
-	
-	@SuppressWarnings("unchecked")
-	public void update(Graphics g) {
-		//create an image off-screen
-		imgOff = createImage(Game.DIM.width, Game.DIM.height);
-		//get its graphics context
-		grpOff = imgOff.getGraphics();
+    // ==============================================================
+    // FIELDS
+    // ==============================================================
 
-		//Fill the off-screen image background with black.
-		grpOff.setColor(Color.black);
-		grpOff.fillRect(0, 0, Game.DIM.width, Game.DIM.height);
+    // The following "off" vars are used for the off-screen double-buffered image.
+    private Image imgOff;
+    private Graphics grpOff;
 
-		drawScore(grpOff);
-		
-		if (CommandCenter.getInstance().isGameOver()) {
-			displayTextOnScreen();
-		} else if (CommandCenter.getInstance().isPaused()) {
-			strDisplay = "Game Paused";
-			grpOff.drawString(strDisplay,
-					(Game.DIM.width - fmt.stringWidth(strDisplay)) / 2, Game.DIM.height / 4);
-		}
-		
-		//playing and not paused!
-		else {
-
-			iterateMovables(grpOff,
-				CommandCenter.getInstance().getMovDebris(),
-				CommandCenter.getInstance().getMovFloaters(),
-				CommandCenter.getInstance().getMovFoes(),
-				CommandCenter.getInstance().getMovFriends());
+    private GameFrame gmf;
+    private Font fnt = new Font("SansSerif", Font.BOLD, 12);
+    private Font fntBig = new Font("SansSerif", Font.BOLD + Font.ITALIC, 36);
+    private FontMetrics fmt;
+    private int fontWidth;
+    private int fontHeight;
+    private String strDisplay = "";
 
 
-			drawNumberShipsLeft(grpOff);
+    // ==============================================================
+    // CONSTRUCTOR
+    // ==============================================================
+
+    public GamePanel(Dimension dim) {
+        gmf = new GameFrame();
+        gmf.getContentPane().add(this);
+        gmf.pack();
+        initView();
+
+        gmf.setSize(dim);
+        gmf.setTitle("Game Base");
+        gmf.setResizable(false);
+        gmf.setVisible(true);
+        setFocusable(true);
+    }
 
 
-		}
+    // ==============================================================
+    // METHODS
+    // ==============================================================
 
-		//after drawing all the movables or text on the offscreen-image, copy it in one fell-swoop to graphics context
-		// of the game panel, and show it for ~40ms. If you attempt to draw sprites directly on the gamePanel, e.g.
-		// without the use of a double-buffered off-screen image, you will see flickering.
-		g.drawImage(imgOff, 0, 0, this);
-	} 
+    private void drawScore(Graphics g) {
+        g.setColor(Color.white);
+        g.setFont(fnt);
+        if (CommandCenter.getInstance().getScore() != 0) {
+            g.drawString("SCORE :  " + CommandCenter.getInstance().getScore(), fontWidth, fontHeight);
+        } else {
+            g.drawString("NO SCORE", fontWidth, fontHeight);
+        }
+    }
 
+    @SuppressWarnings("unchecked")
+    public void update(Graphics g) {
+        //create an image off-screen
+        imgOff = createImage(Game.DIM.width, Game.DIM.height);
+        //get its graphics context
+        grpOff = imgOff.getGraphics();
 
-	
+        //Fill the off-screen image background with black.
+        grpOff.setColor(Color.black);
+        grpOff.fillRect(0, 0, Game.DIM.width, Game.DIM.height);
 
-	@SafeVarargs
-	private final void iterateMovables(final Graphics g, List<Movable>... arrayOfListMovables){
+        drawScore(grpOff);
 
-		BiConsumer<Graphics, Movable> moveDraw = (grp, mov) -> {
-			mov.move();
-			mov.draw(grp);
-		};
+        if (CommandCenter.getInstance().isGameOver()) {
+            displayTextOnScreen(grpOff,
+                    "GAME OVER",
+                    "use the arrow keys to turn and thrust",
+                    "use the space bar to fire",
+                    "'S' to Start",
+                    "'P' to Pause",
+                    "'Q' to Quit",
+                    "left pinkie on 'A' for Shield",
+                    "'Numeric-Enter' for Hyperspace"
 
-		//we use flatMap to flatten the List<Movable>[] passed-in above into a single stream of Movables
-		Arrays.stream(arrayOfListMovables) //Stream<List<Movable>>
-				.flatMap(Collection::stream) //Stream<Movable>
-				.forEach(m -> moveDraw.accept(g, m));
+            );
+        } else if (CommandCenter.getInstance().isPaused()) {
+            strDisplay = "Game Paused";
+            grpOff.drawString(strDisplay,
+                    (Game.DIM.width - fmt.stringWidth(strDisplay)) / 2, Game.DIM.height / 4);
+        }
 
-		
-	}
+        //playing and not paused!
+        else {
 
-
-	private void drawNumberShipsLeft(Graphics g){
-		int numFalcons = CommandCenter.getInstance().getNumFalcons();
-		while (numFalcons > 0){
-			drawOneShipLeft(g, numFalcons--);
-		}
-	}
-
-	// Draw the number of falcons left on the bottom-right of the screen. Upside-down, but ok.
-	private void drawOneShipLeft(Graphics g, int offSet) {
-		Falcon falcon = CommandCenter.getInstance().getFalcon();
-
-		g.setColor(falcon.getColor());
-
-		g.drawPolygon(
-					Arrays.stream(falcon.getCartesians())
-							.map(pnt -> pnt.x + Game.DIM.width - (20 * offSet))
-							.mapToInt(Integer::intValue)
-							.toArray(),
-
-					Arrays.stream(falcon.getCartesians())
-							.map(pnt -> pnt.y + Game.DIM.height - 40)
-							.mapToInt(Integer::intValue)
-							.toArray(),
-
-					falcon.getCartesians().length);
-
-
-
-	}
-	
-	private void initView() {
-		Graphics g = getGraphics();			// get the graphics context for the panel
-		g.setFont(fnt);						// take care of some simple font stuff
-		fmt = g.getFontMetrics();
-		fontWidth = fmt.getMaxAdvance();
-		fontHeight = fmt.getHeight();
-		g.setFont(fntBig);					// set font info
-	}
+            iterateMovables(grpOff,
+                    CommandCenter.getInstance().getMovDebris(),
+                    CommandCenter.getInstance().getMovFloaters(),
+                    CommandCenter.getInstance().getMovFoes(),
+                    CommandCenter.getInstance().getMovFriends());
 
 
-	// This method draws some text to the middle of the screen before/after a game
-	private void displayTextOnScreen() {
+            drawNumberShipsLeft(grpOff);
 
-		placeString(grpOff, "GAME OVER", 0);
-		placeString(grpOff, "use the arrow keys to turn and thrust", 40);
-		placeString(grpOff, "use the space bar to fire", 80);
-		placeString(grpOff, "'S' to Start", 120);
-		placeString(grpOff, "'P' to Pause", 160);
-		placeString(grpOff, "'Q' to Quit", 200);
-		placeString(grpOff, "left pinkie on 'A' for Shield", 240);
-		placeString(grpOff, "'Numeric-Enter' for Hyperspace", 280);
 
-	}
+        }
 
-	private void placeString(Graphics grp, String str, int height){
-		grp.drawString(str, (Game.DIM.width - fmt.stringWidth(str)) / 2, Game.DIM.height / 4 + fontHeight + height);
-	}
+        //after drawing all the movables or text on the offscreen-image, copy it in one fell-swoop to graphics context
+        // of the game panel, and show it for ~40ms. If you attempt to draw sprites directly on the gamePanel, e.g.
+        // without the use of a double-buffered off-screen image, you will see flickering.
+        g.drawImage(imgOff, 0, 0, this);
+    }
 
+
+    @SafeVarargs
+    private final void iterateMovables(final Graphics g, List<Movable>... arrayOfListMovables) {
+
+        BiConsumer<Graphics, Movable> moveDraw = (grp, mov) -> {
+            mov.move();
+            mov.draw(grp);
+        };
+
+        //we use flatMap to flatten the List<Movable>[] passed-in above into a single stream of Movables
+        Arrays.stream(arrayOfListMovables) //Stream<List<Movable>>
+                .flatMap(Collection::stream) //Stream<Movable>
+                .forEach(m -> moveDraw.accept(g, m));
+
+
+    }
+
+
+    private void drawNumberShipsLeft(Graphics g) {
+        int numFalcons = CommandCenter.getInstance().getNumFalcons();
+        while (numFalcons > 0) {
+            drawOneShipLeft(g, numFalcons--);
+        }
+    }
+
+    // Draw the number of falcons left on the bottom-right of the screen. Upside-down, but ok.
+    private void drawOneShipLeft(Graphics g, int offSet) {
+        Falcon falcon = CommandCenter.getInstance().getFalcon();
+
+        g.setColor(falcon.getColor());
+
+        g.drawPolygon(
+                Arrays.stream(falcon.getCartesians())
+                        .map(pnt -> pnt.x + Game.DIM.width - (20 * offSet))
+                        .mapToInt(Integer::intValue)
+                        .toArray(),
+
+                Arrays.stream(falcon.getCartesians())
+                        .map(pnt -> pnt.y + Game.DIM.height - 40)
+                        .mapToInt(Integer::intValue)
+                        .toArray(),
+
+                falcon.getCartesians().length);
+
+
+    }
+
+    private void initView() {
+        Graphics g = getGraphics();            // get the graphics context for the panel
+        g.setFont(fnt);                        // take care of some simple font stuff
+        fmt = g.getFontMetrics();
+        fontWidth = fmt.getMaxAdvance();
+        fontHeight = fmt.getHeight();
+        g.setFont(fntBig);                    // set font info
+    }
+
+
+    // This method draws some text to the middle of the screen before/after a game
+    private void displayTextOnScreen(final Graphics graphics, String... lines) {
+
+        AtomicInteger spacer = new AtomicInteger(40);
+        Arrays.stream(lines)
+                .forEach(s -> {
+                            graphics.drawString(s, (Game.DIM.width - fmt.stringWidth(s)) / 2,
+                                    Game.DIM.height / 4 + fontHeight + spacer.getAndAdd(40));
+                        }
+                );
+
+
+    }
 
 
 }
