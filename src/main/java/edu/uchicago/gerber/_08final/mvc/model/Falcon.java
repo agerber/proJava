34321642c41b9f80cbf115cc9f405477222f71a2
Adam Gerber <adam.gerber@yahoo.com>
2022-11-27 10:30:39 -0600

    correction

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/Falcon.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/Falcon.java
@@ -17,7 +17,7 @@ public class Falcon extends Sprite {
 	private final static int DEGREE_STEP = 9;
 	public static final int INITIAL_SPAWN_TIME = 68;
 
-	//a counter which counts down from INITIAL_SPAWN_TIME to zero at each move(). Used for determining protection
+	//a counter which counts down from INITIAL_SPAWN_TIME to zero (see move()). Used for determining protection
 	private int spawn;
 
 	private boolean thrusting = false;
