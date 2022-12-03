fd70176893642e0ea369351016352e24f1016f70
Adam Gerber <adam.gerber@yahoo.com>
2022-12-03 15:42:37 -0600

    comments

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/NewWallFloater.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/NewWallFloater.java
@@ -5,7 +5,7 @@ import edu.uchicago.gerber._08final.mvc.controller.Game;
 import java.awt.*;
 
 public class NewWallFloater extends Floater {
-	//spawn every 40 seconds
+	//spawn every 40 seconds (so long as there are no bricks)
 	public static final int SPAWN_NEW_WALL_FLOATER = Game.FRAMES_PER_SECOND * 40;
 	public NewWallFloater() {
 		setColor(Color.ORANGE);
