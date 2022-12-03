a90e9d0134150ffb720d22ab068c436d64440d96
Adam Gerber <adam.gerber@yahoo.com>
2022-12-03 09:50:30 -0600

    comments

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/ShieldFloater.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/ShieldFloater.java
@@ -5,6 +5,7 @@ import edu.uchicago.gerber._08final.mvc.controller.Game;
 import java.awt.*;
 
 public class ShieldFloater extends Floater {
+	//spawn every 25 seconds
 	public static final int SPAWN_SHIELD_FLOATER = Game.FRAMES_PER_SECOND * 25;
 	public ShieldFloater() {
 		setColor(Color.CYAN);
