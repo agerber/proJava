8121bff8b66cfa8bd1681588b3dc5b0a113dcf6b
Adam Gerber <adam.gerber@yahoo.com>
2022-12-12 12:12:03 -0500

    override move

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/Brick.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/Brick.java
@@ -44,4 +44,11 @@ public class Brick extends Sprite {
 		//g.drawOval(getCenter().x - getRadius(), getCenter().y - getRadius(), getRadius() *2, getRadius() *2);
 	}
 
+	//the reason we override the move method is to skip the logic contained in super-class Sprite
+	//and gaining slight performance
+	@Override
+	public void move(){
+		//do NOT call super and do nothing, a brick does not move.
+	}
+
 } //end class
