1d224c5da31c7bd0dd974fd4ee38e5294f26bc31
Adam Gerber <adam.gerber@yahoo.com>
2023-01-14 20:29:08 -0500

    comments

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/PolarPoint.kt
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/PolarPoint.kt
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor
 import lombok.Data
 
 data class PolarPoint(
-    //we use the wrapper-class Double as members to get the Comparable interface
-    //because Asteroid needs to sort by theta when generating random-shapes.
     // corresponds to the hypotenuse in cartesean, number between 0 and 1
     val r: Double,
     //degrees in radians, number between 0 and 6.283
