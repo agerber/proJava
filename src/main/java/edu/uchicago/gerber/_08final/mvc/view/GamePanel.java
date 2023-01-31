a8addba5384190602962fe25b09f43c3bd663036
Adam Gerber <adam.gerber@yahoo.com>
2023-01-31 13:51:38 -0500

    more comments

--- src/main/java/edu/uchicago/gerber/_08final/mvc/view/GamePanel.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/view/GamePanel.java
@@ -262,7 +262,7 @@ public class GamePanel extends Panel {
     }
 
 
-    // This method draws some text to the middle of the screen before/after a game
+    // This method draws some text to the middle of the screen
     private void displayTextOnScreen(final Graphics graphics, String... lines) {
 
         //AtomicInteger is safe to pass into a stream
