fa5dd4ea84a6b5ac1cb5298ea120a7e5dd9ff785
Adam Gerber <adam.gerber@yahoo.com>
2023-02-16 11:31:59 -0500

    rename precision

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/Asteroid.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/Asteroid.java
@@ -79,11 +79,11 @@ public class Asteroid extends Sprite {
 		  final int MAX_RADIANS_X1000 =6283;
 		  //when casting from double to int, we truncate and lose precision, so best to be generous with the
 		  //precision factor as this will create a more random distribution of vertices
-		  final double PRECISION = 1000.0;
+		  final double PRECISION = 100.0;
 
 		  Supplier<PolarPoint> polarPointSupplier = () -> {
-			  double r = (800 + Game.R.nextInt(200)) / PRECISION; //number between 0.8 and 0.999
-			  double theta = Game.R.nextInt(MAX_RADIANS_X1000) / PRECISION; // number between 0 and 6.282
+			  double r = (800 + Game.R.nextInt(200)) / 1000.0; //number between 0.8 and 0.999
+			  double theta = Game.R.nextInt(MAX_RADIANS_X1000) / 1000.0; // number between 0 and 6.282
 		  	  return new PolarPoint(r, theta);
 		  };
 
