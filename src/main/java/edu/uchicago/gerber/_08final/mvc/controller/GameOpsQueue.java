ab1bb87912320645a8ffebe69574d1bd977ae983
Adam Gerber <adam.gerber@yahoo.com>
2023-02-19 07:01:08 -0500

    comments on GameOpsQueue.java

--- src/main/java/edu/uchicago/gerber/_08final/mvc/controller/GameOpsQueue.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/controller/GameOpsQueue.java
@@ -13,15 +13,16 @@ import java.util.concurrent.locks.ReentrantLock;
 public class GameOpsQueue extends LinkedList<GameOp> {
 
     //this data structure is in contention by the "Event Dispatch" thread aka main-swing-thread, and the animation
-    // thread. We must restrict access to it by one thread at a time by using a Lock.
+    // thread.  For example, enqueues may happen either as a result of a keystroke such as a bullet-fire
+    // (main-swing-thread) or a collision (animation-thread). We must restrict access to it by one thread at a
+    // time by using a Lock.
     private final Lock lock;
 
     public GameOpsQueue() {
         lock =   new ReentrantLock();
     }
 
-    //enqueues may happen either as a result of a keystroke such as a bullet fire (main-swing-thread) or
-    // a collision, etc. (animation-thread). Use a lock to ensure this data structure is thread safe.
+
     public void enqueue(Movable mov, GameOp.Action action) {
        try {
             lock.lock();
@@ -32,8 +33,6 @@ public class GameOpsQueue extends LinkedList<GameOp> {
     }
 
 
-    //dequeues are done in serial by the animation thread. However, we don't want to attempt to enqueue and
-    //dequeue the same datastructure simultaneously. Make thread-safe.
     public GameOp dequeue() {
         try {
             lock.lock();
