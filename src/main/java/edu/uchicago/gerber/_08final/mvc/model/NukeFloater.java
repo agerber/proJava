cdfc3d369918f48411932ea00a6c81e11d38160e
Adam Gerber <adam.gerber@yahoo.com>
2023-03-26 20:13:26 -0400

    add nuke fl

---
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/NukeFloater.java
@@ -0,0 +1,16 @@
+package edu.uchicago.gerber._08final.mvc.model;
+
+import edu.uchicago.gerber._08final.mvc.controller.Game;
+
+import java.awt.*;
+
+public class NukeFloater extends Floater {
+
+	public static final int SPAWN_NUKE_FLOATER = Game.FRAMES_PER_SECOND * 10;
+	public NukeFloater() {
+		setColor(Color.RED);
+		setExpiry(120);
+	}
+
+
+}
