35d4a6c8f96ece4309c6115a8ba601e1ce3eb899
Adam Gerber <adam.gerber@yahoo.com>
2023-03-27 00:03:13 -0400

    nuke logic

--- src/main/java/edu/uchicago/gerber/_08final/mvc/model/Nuke.java
+++ src/main/java/edu/uchicago/gerber/_08final/mvc/model/Nuke.java
@@ -5,15 +5,28 @@ import java.awt.*;
 
 public class Nuke extends Sprite{
 
-    private final int MAX_COUNT = 40;
-    private int count;
+    private final int EXPIRE = 60;
+    private int count = 0;
 
     public Nuke(Falcon falcon) {
         setCenter(falcon.getCenter());
         setColor(Color.YELLOW);
-        setExpiry(MAX_COUNT);
+        setExpiry(EXPIRE);
         setRadius(0);
         setTeam(Team.FRIEND);
+        //the nuke is a cicle, so it has no orientation, but we need this to calculate vectors
+        setOrientation(falcon.getOrientation());
+
+        final double FIRE_POWER = 11.0;
+        double vectorX =
+                Math.cos(Math.toRadians(getOrientation())) * FIRE_POWER;
+        double vectorY =
+                Math.sin(Math.toRadians(getOrientation())) * FIRE_POWER;
+
+        //fire force: falcon inertia + fire-vector
+        setDeltaX(falcon.getDeltaX() + vectorX);
+        setDeltaY(falcon.getDeltaY() + vectorY);
+
     }
 
 
@@ -25,14 +38,32 @@ public class Nuke extends Sprite{
 
     }
 
+    //a nuke is invincible while it hasn't yet expired
+    @Override
+    public boolean isProtected() {
+        return true;
+    }
+
     @Override
     public void move() {
         super.move();
+        if (getExpiry() % 20 == 0) count++;
+        switch (count) {
+            case 0:
+            default:
+                setRadius(2);
+                break;
+            case 1:
+                setRadius(getRadius() + 16);
+                break;
+            case 2:
+                setRadius(getRadius() - 16);
+                break;
 
-        if (count++ <= MAX_COUNT / 2)
-            setRadius(getRadius() + 16);
-        else
-            setRadius(getRadius() -16);
+
         }
+
+    }
+
 
 }
